f8bb570c7d1cf0511060cb6793482ff809e7e5ec
philloooo <phillis.tt@gmail.com>
2016-09-22 15:55:09 -0500

    feat(cwl): remove cwl blueprint

--- scheduler/__init__.py
+++ scheduler/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 from .job import blueprint as job
-from .cwl import blueprint as cwl
 from .resources.cwl import CWLLibrary
 from logging import log_request, log_response_code
 from errors import APIError
@@ -11,13 +10,11 @@ app = Flask(__name__)
 
 def app_init(app, settings="scheduler.settings"):
     print settings
-    app.register_blueprint(job, url_prefix='/job')
-    app.register_blueprint(cwl, url_prefix='/cwl')
+    app.register_blueprint(job, url_prefix='/jobs')
     app.config.from_object(settings)
     app.url_map.strict_slashes = False
     app.db = SQLAlchemyDriver(app.config['DB'])
     app.cwl = CWLLibrary(app.config['ALLOWED_DOCKER_REGISTRIES'])
-
 
 
 @app.before_request
@@ -32,7 +29,7 @@ def after_req(response):
 
 @app.route('/')
 def root():
-    return jsonify({'job endpoint': '/job', 'cwl endpoint': '/cwl'})
+    return jsonify({'job endpoint': '/jobs'})
 
 
 @app.errorhandler(APIError)
