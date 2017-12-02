38d41ff71348add2ce68d1191709943282e163fb
Kyle Hernandez <khernandez@bsd.uchicago.edu>
2017-12-02 04:05:30 +0000

    feat(dbcreds): handle dbcreds

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -1,5 +1,9 @@
 AUTH = True 
-DB = 'postgresql://test:test@localhost/schedulerapi'
+#DB = 'postgresql://test:test@localhost/schedulerapi'
+# KMH: need to change this for my slurm test
+with open('psql', 'rt') as fh:
+    DB = fh.read().rstrip()
+ 
 PROXIES = True
 
 ALLOWED_DOCKER_REGISTRIES = [
