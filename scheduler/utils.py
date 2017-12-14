7c0918ecc16cca33d34fad2f5a6ce6e356ada9a0
Kyle Hernandez <khernandez@bsd.uchicago.edu>
2017-12-14 18:44:03 +0000

    feat(utils): add uuid gen and sha

--- scheduler/utils.py
+++ scheduler/utils.py
@@ -1,6 +1,9 @@
 from .models.models import User
 from cdispyutils.log import get_logger
 import bcrypt
+import hashlib
+import json
+import uuid
 
 logger = get_logger('utils')
 
@@ -13,3 +16,11 @@ def create_user(db, username, password):
     except:
         logger.exception("Fail to create user")
         return False
+
+def get_payload_hash(payload):
+    pay_str = json.dumps(payload, sort_keys=True) 
+    sha = hashlib.sha256(pay_str).hexdigest()
+    return sha
+
+def uuidstr(): 
+    return str(uuid.uuid4()) 
